2b44874236b6f2a58ab0772aff73d878028a592b
Rob Miguel <robmiguel2@gmail.com>
2020-01-18 14:19:30 -0700

    add tableview

---
+++ testApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Rob Miguel on 1/18/20.
+//  Copyright © 2020 Rob Miguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
