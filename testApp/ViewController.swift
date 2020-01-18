c1f1bf6be944f0431c4504f0112cdb6033c9dadb
Rob Miguel <robmiguel2@gmail.com>
2020-01-18 14:42:19 -0700

    configured tableview cell

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1000
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let myCell = tableView.dequeueReusableCell(withIdentifier: "normalCell", for: indexPath)
         
         myCell.textLabel?.text = "This is row number \(indexPath.row + 1)"
+        myCell.accessoryType = .disclosureIndicator
+        myCell.imageView?.image = UIImage(systemName: "paperplane.fill")
+        myCell.detailTextLabel?.text = "This is just some detail"
         
         return myCell
     }
